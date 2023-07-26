a77c4e5dda13392e17c98a50cf9f0559a9ebd4cb
preetlakra <lakraaman007@gmail.comm>
2023-07-26 18:50:08 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,66 @@
-const Hello = (props) => {
-  // here i write functions
+import React from 'react';
+
+const Header = (props) =>{
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) =>{
+  const part1 =  props.parts[0]
+  const part2 =  props.parts[1]
+  const part3 =  props.parts[2]
+  
+  return (
+    <div>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
+    
+    </div>
+    
+  )
+ 
+}
+
+const Total = (props) =>{
+
+  const totalExercises = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises 
 
 
   return (
-    // here i call the functions 
     <div>
-      
-      <p>Hello {props.name},  You are {props.age} years old. </p>
+      <p>Number of exercises {totalExercises} </p>
     </div>
   )
 }
 
 
-//Main Component
 const App = () => {
-  
+  // const-definitions
+  const course = "Half Stack Application Development"
+
+  const parts =[
+    {name:'Fundamentals of React', exercises: 10},
+    {name:'Using props to pass data',exercises: 7},
+    {name:'State of a component',exercises: 14}
+  ]
 
   return (
     <div>
 
-      <h1>Greetings !</h1>
+      <Header course = {course} />
+      <Content parts= {parts} />
+      <Total parts= {parts} />
 
-      <Hello name="Preet Lakra" age='30' />
-      
-    
-      
     </div>
   )
 }
